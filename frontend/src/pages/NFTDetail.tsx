7cb6cc151d4f99e62eaed9767de9109115ef3a82
rudye223 <39869067+rudye223@users.noreply.github.com>
2024-12-19 04:18:33 +0000

    changes

--- frontend/src/pages/NFTDetail.tsx
+++ frontend/src/pages/NFTDetail.tsx
@@ -1,38 +1,67 @@
 import React, { useState, useEffect } from "react";
-import { Button, Input, Card, Spin, message, Modal, Row, Col, Typography, Tag } from "antd";
+import {
+  Button,
+  Card,
+  Spin,
+  message,
+  Row,
+  Col,
+  Typography,
+  Tag,
+  Tooltip,
+} from "antd";
 import { AptosClient } from "aptos";
 import { useParams } from "react-router-dom";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { MARKET_PLACE_ADDRESS, MARKET_PLACE_NAME } from "../Constants";
-import { CheckOutlined, EditOutlined, DollarCircleOutlined, ClockCircleOutlined } from '@ant-design/icons';
+import {
+  DollarCircleOutlined,
+  ClockCircleOutlined,
+  UserOutlined
+} from "@ant-design/icons";
 import ConfirmPurchaseModal from "../components/ConfirmPurchaseModal";
 import PlaceBidModal from "../components/PlaceBidModal";
 import StartAuctionModal from "../components/StartAuctionModal";
 import ListForSaleModal from "../components/ListForSaleModal";
-import { fetchNFTDataUtil } from "../utils/fetchNFTData";  
-import { rarityColors, rarityLabels } from "../utils/rarityUtils";  
-
-  
+import { fetchNFTDataUtil } from "../utils/fetchNFTData";
+import { rarityColors, rarityLabels } from "../utils/rarityUtils";
 
 const { Title, Paragraph, Text } = Typography;
 const client = new AptosClient("https://fullnode.devnet.aptoslabs.com/v1");
- 
+
 
 const NFTDetail: React.FC = () => {
   const { tokenId } = useParams<{ tokenId: string }>();
   const [nftDetails, setNftDetails] = useState<any>(null);
   const [auctionData, setAuctionData] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  
+
   const { account } = useWallet();
   const [isListModalVisible, setIsListModalVisible] = useState(false);
-  
+
   const [isAuctionModalVisible, setIsAuctionModalVisible] = useState(false);
- 
+
   const [isBidModalVisible, setIsBidModalVisible] = useState(false);
   const [isBuyModalVisible, setIsBuyModalVisible] = useState(false);
   const [countdown, setCountdown] = useState<string>("");
 
+    // Custom styles for the card
+  const cardStyle = {
+      maxWidth: '800px',
+      width: '100%',
+      margin: '20px auto',
+      borderRadius: '12px',
+      boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+      overflow: 'hidden',
+  };
+
+  const imageStyle = {
+        width: '100%',
+        height: 'auto',
+        display: 'block',
+        objectFit: 'cover',
+        borderRadius: '12px 0 0 12px',
+    };
 
 
   useEffect(() => {
@@ -50,8 +79,12 @@ const NFTDetail: React.FC = () => {
           clearInterval(interval);
           setCountdown("Auction expired");
         } else {
-          const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+          const hours = Math.floor(
+            (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+          );
+          const minutes = Math.floor(
+            (timeLeft % (1000 * 60 * 60)) / (1000 * 60)
+          );
           const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
           setCountdown(`${hours}h ${minutes}m ${seconds}s`);
         }
@@ -62,9 +95,8 @@ const NFTDetail: React.FC = () => {
   }, [auctionData]);
 
   const fetchNFTData = async () => {
-
-    if(!tokenId) return;
-    if(!account) return;
+    if (!tokenId) return;
+    if (!account) return;
     setLoading(true);
     const data = await fetchNFTDataUtil(tokenId, account?.address, client);
     if (data) {
@@ -75,28 +107,30 @@ const NFTDetail: React.FC = () => {
     }
     setLoading(false);
   };
-   
+
   const handleEndSale = async () => {
     if (!account) return;
     try {
-    const entryFunctionPayload = {
-            type: "entry_function_payload",
-            function: `${MARKET_PLACE_ADDRESS}::${MARKET_PLACE_NAME}::end_sale`,
-            type_arguments: [],
-            arguments: [MARKET_PLACE_ADDRESS, nftDetails.id.toString() ],
-          };
-    
-          const response = await (window as any).aptos.signAndSubmitTransaction(entryFunctionPayload);
-          await client.waitForTransaction(response.hash);
-    
-          message.success("NFT sale ended successfully!");
-          setNftDetails(null)
+      const entryFunctionPayload = {
+        type: "entry_function_payload",
+        function: `${MARKET_PLACE_ADDRESS}::${MARKET_PLACE_NAME}::end_sale`,
+        type_arguments: [],
+        arguments: [MARKET_PLACE_ADDRESS, nftDetails.id.toString()],
+      };
 
-          await fetchNFTData()
-        } catch (error) {
-          console.error("Error ending NFT sale:", error);
-          message.error("Failed to end auction.");
-        }
+      const response = await (
+        window as any
+      ).aptos.signAndSubmitTransaction(entryFunctionPayload);
+      await client.waitForTransaction(response.hash);
+
+      message.success("NFT sale ended successfully!");
+      setNftDetails(null);
+
+      await fetchNFTData();
+    } catch (error) {
+      console.error("Error ending NFT sale:", error);
+      message.error("Failed to end auction.");
+    }
   };
 
   const handleEndAuction = async (nftId: number) => {
@@ -109,174 +143,212 @@ const NFTDetail: React.FC = () => {
         arguments: [MARKET_PLACE_ADDRESS, nftId],
       };
 
-      const txnResponse = await (window as any).aptos.signAndSubmitTransaction(entryFunctionPayload);
+      const txnResponse = await (
+        window as any
+      ).aptos.signAndSubmitTransaction(entryFunctionPayload);
       console.log("Transaction Response:", txnResponse);
       await client.waitForTransaction(txnResponse.hash);
       message.success(`Auction ended successfully!`);
-      setAuctionData(null)
-      await fetchNFTData()
+      setAuctionData(null);
+      await fetchNFTData();
     } catch (error) {
       console.error("Error ending auction:", error);
       message.error("Failed to end auction.");
     }
   };
-   
-  const handleBuyClick = () => {
 
+  const handleBuyClick = () => {
     setIsBuyModalVisible(true);
   };
 
-if (loading) {
+  if (loading) {
     return (
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
         <Spin size="large" />
-
       </div>
     );
   }
 
   return (
-    <div style={{ padding: '20px', display: "flex", flexDirection:"column", alignItems: "center", justifyContent: "center" }}>
-       <Title level={4} style={{ marginBottom: "20px", textAlign:"center" }}>NFT Details</Title>
+    <div
+        style={{
+            padding: '20px',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center"
+        }}
+    >
+      <Title level={2} style={{ marginBottom: "20px", textAlign: "center" }}>
+        NFT Details
+      </Title>
       {nftDetails && (
-       <Card style={{ width: '100%', marginBottom: 20 }}>
-       <Row gutter={[16, 16]}>
-         {/* Left Column: Cover Image */}
-         <Col xs={24} sm={10} md={8}>
-           <img
-             alt={nftDetails.name}
-             src={nftDetails.uri}
-             style={{ width: '100%', borderRadius: '8px' }}
-           />
-         </Col>
-     
-         {/* Right Column: Details */}
-         <Col xs={24} sm={14} md={16}>
-           <Title level={3}>{nftDetails.name}</Title>
-           <Tag
-             color={rarityColors[nftDetails.rarity]}
-             style={{ fontSize: '14px', fontWeight: 'bold', marginBottom: '10px' }}
-           >
-             {rarityLabels[nftDetails.rarity]}
-           </Tag>
-     
-           <Paragraph style={{ margin: '5px 0', fontSize: '16px' }}>
-             <Text strong>NFT ID:</Text> {nftDetails.id}
-           </Paragraph>
-           <Paragraph style={{ margin: '5px 0', fontSize: '16px' }}>
-             <Text strong>Price:</Text> {auctionData ? `Auction` : `${nftDetails.price} APT`}
-           </Paragraph>
-           <Paragraph style={{ margin: '5px 0', fontSize: '16px' }}>
-             <Text strong>Rarity:</Text> {rarityLabels[nftDetails.rarity]}
-           </Paragraph>
-           <Paragraph style={{ margin: '5px 0', fontSize: '16px' }}>
-             <Text strong>For Sale:</Text> {nftDetails.for_sale ? 'Yes' : 'No'}
-           </Paragraph>
-           <Paragraph style={{ marginTop: '20px', fontSize: '16px', lineHeight: 1.5 }}>
-             <Text strong>Description:</Text> {nftDetails.description}
-           </Paragraph>
-           <Paragraph style={{ marginTop: '20px', fontSize: '13px', lineHeight: 1.5 }}>
-             <Text strong>Owner:</Text> {nftDetails.owner === account?.address && "You | "}{nftDetails.owner}
-           </Paragraph>
-     
-           {auctionData && (
-             <>
-               <hr />
-               <Title level={4}>Auction Information</Title>
-               <Paragraph>
-                 <Text strong>Auction End Time:</Text> {new Date(auctionData.end_time * 1000).toLocaleString()}
-               </Paragraph>
-               <Paragraph>
-                 <Text strong>End Countdown:</Text> <span style={{ color: "red" }}>{countdown}</span>
-               </Paragraph>
-               <Paragraph>
-                 <Text strong>Starting Bid:</Text> {auctionData.starting_price} APT
-               </Paragraph>
-               <Paragraph>
-                 <Text strong>Highest Bid:</Text> {auctionData.highest_bid} APT
-               </Paragraph>
-             </>
-           )}
-     
-           <Row gutter={16} style={{ marginTop: '20px' }}>
-             <Col span={12}>
-               {auctionData ? (
-                 nftDetails.owner === account?.address ? (
-                   <Button
-                     type="primary"
-                     danger
-                     block
-                     onClick={() => handleEndAuction(nftDetails.id)}
-                     icon={<ClockCircleOutlined />}
-                   >
-                     End Auction
-                   </Button>
-                 ) : (
-                   <Button
-                     disabled={auctionData.isExpired}
-                     type="primary"
-                     block
-                     onClick={() => setIsBidModalVisible(true)}
-                     icon={<DollarCircleOutlined />}
-                   >
-                     Place Bid
-                   </Button>
-                 )
-               ) : (
-                 nftDetails.for_sale ? (
-                   nftDetails.owner === account?.address ? (
-                     <Button
-                       type="primary"
-                       danger
-                       block
-                       onClick={handleEndSale}
-                       icon={<DollarCircleOutlined />}
-                     >
-                       End Sale
-                     </Button>
-                   ) : (
-                     <Button type="primary" onClick={() => handleBuyClick()}>
-                       Buy
-                     </Button>
-                   )
-                 ) : (
-                   nftDetails.owner === account?.address && (
-                     <Button
-                       type="primary"
-                       block
-                       onClick={() => setIsListModalVisible(true)}
-                       icon={<DollarCircleOutlined />}
-                     >
-                       List for Sale
-                     </Button>
-                   )
-                 )
-               )}
-             </Col>
-             <Col span={12}>
-               {!auctionData ? (
-                 nftDetails.owner === account?.address && (
-                   <Button
-                     type="primary"
-                     block
-                     onClick={() => setIsAuctionModalVisible(true)}
-                     icon={<ClockCircleOutlined />}
-                     disabled={nftDetails.for_sale}
-                   >
-                     Start Auction
-                   </Button>
-                 )
-               ) : null}
-             </Col>
-           </Row>
-         </Col>
-       </Row>
-     </Card>
-     
+        <Card style={cardStyle}>
+          <Row gutter={[16, 16]}>
+            {/* Left Column: Cover Image */}
+            <Col xs={24} sm={10} md={8}>
+              <img alt={nftDetails.name} src={nftDetails.uri} style={imageStyle} />
+            </Col>
+
+            {/* Right Column: Details */}
+            <Col xs={24} sm={14} md={16} style={{ paddingLeft: '20px'}}>
+              <Title level={3} style={{marginBottom: '10px'}}>{nftDetails.name}</Title>
+               <Tag
+                    color={rarityColors[nftDetails.rarity]}
+                    style={{
+                        fontSize: '16px',
+                        fontWeight: 'bold',
+                        marginBottom: '15px',
+                        padding: '5px 10px',
+                        borderRadius: '8px',
+                    }}
+                >
+                    {rarityLabels[nftDetails.rarity]}
+                </Tag>
+
+              <Paragraph style={{ margin: "5px 0", fontSize: "16px" }}>
+                <Text strong>NFT ID:</Text> {nftDetails.id}
+              </Paragraph>
+              <Paragraph style={{ margin: "5px 0", fontSize: "16px" }}>
+                <Text strong>Price:</Text>{" "}
+                {auctionData ? `Auction` : `${nftDetails.price} APT`}
+              </Paragraph>
+                <Paragraph style={{ margin: '5px 0', fontSize: '16px' }}>
+                    <Text strong>Rarity:</Text> {rarityLabels[nftDetails.rarity]}
+                </Paragraph>
+              <Paragraph style={{ margin: "5px 0", fontSize: "16px" }}>
+                <Text strong>For Sale:</Text> {nftDetails.for_sale ? "Yes" : "No"}
+              </Paragraph>
+              <Paragraph
+                style={{ marginTop: "15px", fontSize: "16px", lineHeight: 1.5 }}
+              >
+                <Text strong>Description:</Text> {nftDetails.description}
+              </Paragraph>
+                 <Paragraph style={{ marginTop: '10px', fontSize: '14px', display: 'flex', alignItems: 'center' }}>
+                    <Text strong>Owner:  </Text>
+                    <Tooltip title={nftDetails.owner}>
+                        <Text
+                            style={{
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                                whiteSpace: 'nowrap',
+                                display: 'inline-block',
+                                maxWidth: '400px', // Or any other value for max width
+                                marginLeft: "5px"
+                            }}
+                        >
+                            {nftDetails.owner === account?.address ? "You | " : ""}
+                            <UserOutlined style={{ marginRight: '5px',fontSize: '14px' }}/>{nftDetails.owner}
+                        </Text>
+                    </Tooltip>
+                </Paragraph>
+
+              {auctionData && (
+                <>
+                   <hr style={{ margin: '15px 0' }} />
+                  <Title level={4} style={{ marginBottom: '10px' }}>Auction Information</Title>
+                    <Paragraph style={{margin: "5px 0", fontSize: "16px"}}>
+                    <Text strong>Auction End Time:</Text>{" "}
+                    {new Date(auctionData.end_time * 1000).toLocaleString()}
+                    </Paragraph>
+                    <Paragraph style={{margin: "5px 0", fontSize: "16px"}}>
+                    <Text strong>End Countdown:</Text>{" "}
+                    <span style={{ color: "red" }}>{countdown}</span>
+                    </Paragraph>
+                  <Paragraph style={{margin: "5px 0", fontSize: "16px"}}>
+                    <Text strong>Starting Bid:</Text> {auctionData.starting_price}
+                    APT
+                  </Paragraph>
+                  <Paragraph style={{margin: "5px 0", fontSize: "16px"}}>
+                    <Text strong>Highest Bid:</Text> {auctionData.highest_bid} APT
+                  </Paragraph>
+                </>
+              )}
+
+              <Row gutter={16} style={{ marginTop: "20px" }}>
+                <Col span={12}>
+                  {auctionData ? (
+                    nftDetails.owner === account?.address ? (
+                      <Button
+                        type="primary"
+                        danger
+                        block
+                        onClick={() => handleEndAuction(nftDetails.id)}
+                        icon={<ClockCircleOutlined />}
+                      >
+                        End Auction
+                      </Button>
+                    ) : (
+                      <Button
+                        disabled={auctionData.isExpired}
+                        type="primary"
+                        block
+                        onClick={() => setIsBidModalVisible(true)}
+                        icon={<DollarCircleOutlined />}
+                      >
+                        Place Bid
+                      </Button>
+                    )
+                  ) : nftDetails.for_sale ? (
+                    nftDetails.owner === account?.address ? (
+                      <Button
+                        type="primary"
+                        danger
+                        block
+                        onClick={handleEndSale}
+                        icon={<DollarCircleOutlined />}
+                      >
+                        End Sale
+                      </Button>
+                    ) : (
+                      <Button type="primary" block onClick={() => handleBuyClick()}>
+                        Buy
+                      </Button>
+                    )
+                  ) : (
+                    nftDetails.owner === account?.address && (
+                      <Button
+                        type="primary"
+                        block
+                        onClick={() => setIsListModalVisible(true)}
+                        icon={<DollarCircleOutlined />}
+                      >
+                        List for Sale
+                      </Button>
+                    )
+                  )}
+                </Col>
+                <Col span={12}>
+                  {!auctionData ? (
+                    nftDetails.owner === account?.address && (
+                      <Button
+                        type="primary"
+                        block
+                        onClick={() => setIsAuctionModalVisible(true)}
+                        icon={<ClockCircleOutlined />}
+                        disabled={nftDetails.for_sale}
+                      >
+                        Start Auction
+                      </Button>
+                    )
+                  ) :   
+                    null
+                  }
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Card>
       )}
 
-
-    <ListForSaleModal
+      <ListForSaleModal
         isVisible={isListModalVisible}
         onClose={() => setIsListModalVisible(false)}
         nftDetails={nftDetails}
